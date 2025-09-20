3430b626822949150437b9bd5ed59b2b67dc74b1
Jar-pratyush <rajpratyush0902@gmail.com>
2025-09-20 22:52:48 +0530

    First Commit for the Smart_Office_Assitant_Project

---
+++ main.py
@@ -0,0 +1,46 @@
+def get_email_text():
+    """
+    This function will take the email text from the user as input and return the text as string 
+    """
+
+    email_text = input("Enter the email text: \n")
+
+    return email_text
+
+def get_bullet_points():
+    """
+    This funciton will take the bullet points that must be there in the email in a comma separted format and return the list of points without any whitespaces and removing all points which become empty after stripping the whitespaces.
+    """
+
+    bullet_points = input("Enter the bullet points as comma separated values:\n")
+    points = [point.strip() for point in bullet_points.split(",") if point.strip()]
+    return points
+
+def choose_tone():
+    "Asks the user to choose one of the four tones for input in prompt. The tones are mapped in the form of dictionary."
+
+    tone_options = {1:"formal",2:"friendly",3:"concise",4:"deatiled"}
+    print(f"Choose a tone option: 1:{tone_options.get(1)}, 2:{tone_options.get(2)}, 3:{tone_options.get(3)}, 4:{tone_options.get(4)}")
+    try:
+        user_tone_choice = int(input("Enter a choice 1-4:\n"))
+        return tone_options.get(user_tone_choice,"formal")
+    except ValueError:
+        return "formal"
+    
+def build_prompt(email_text, bullet_points, tone):
+    """
+    Constructing the final prompt using all the inputs
+    """
+    return f"Draft a professional email reply in a {tone} tone based on the following.\n Original email: {email_text}\n Key Points to include: {bullet_points}\n Reply:" 
+
+if __name__ == "__main__":
+    # Get the original text from the user from email
+    email_text = get_email_text()
+    # Get bullet points from the user
+    bullet_points = get_bullet_points()
+    # Get the desired reply tone
+    tone = choose_tone()
+    # Final Prompt
+    prompt = build_prompt(email_text, bullet_points, tone)
+    #Display the generated prompt
+    print("\nGenerated Prompt:\n",prompt)
